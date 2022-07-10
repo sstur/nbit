6cf9ef14fbfbc41ed27da4ff56dd2a74c38b63e4
Simon <sstur@me.com>
2022-07-09 23:59:06 -0700

    Update demo with getContext()

--- src/demo/helpers/application.ts
+++ src/demo/helpers/application.ts
@@ -5,6 +5,19 @@ import { createApplication } from '../../http';
 const { defineRoutes, attachRoutes } = createApplication({
   root: path.resolve(__dirname, '../..'),
   allowStaticFrom: ['assets'],
+  getContext: (request) => ({
+    auth: async () => {
+      const authHeader = request.headers.get('Authorization') ?? '';
+      const token = authHeader.startsWith('Bearer')
+        ? authHeader.split(' ')[1]
+        : undefined;
+      return isValidAuthToken(token);
+    },
+  }),
 });
+
+function isValidAuthToken(token: string | undefined) {
+  return token === 'some-valid-token';
+}
 
 export { defineRoutes, attachRoutes };
