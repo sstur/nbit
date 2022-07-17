44d943496ea41714fd9eb9b3a8f45ea3209ad8a2
Simon <sstur@me.com>
2022-07-16 22:14:53 -0700

    Add note about Request/Response stubs

--- packages/core/src/Request.ts
+++ packages/core/src/Request.ts
@@ -1,3 +1,9 @@
+/**
+ * This is intentionally just a stub; this will never be instantiated because
+ * the files which reference this will only be called from the locations that
+ * symlink to `core` meaning, when they import `../Request` they will be
+ * getting the real Request (from the parent of they symlink) not this stub.
+ */
 export class Request<M extends string, Params extends string> {
   readonly method = '' as M;
   readonly params = {} as { [K in Params]: string };
