bee7a40ffd4a841c226d390e6e07a7b2b6ff684e
Simon <sstur@me.com>
2022-07-17 09:45:17 -0700

    Factor out toReadStream()

--- packages/node/src/support/streams.ts
+++ packages/node/src/support/streams.ts
@@ -1,10 +1,20 @@
-import type { Readable } from 'stream';
+import { Readable } from 'stream';
 import type { ReadableStream } from 'stream/web';
 
-export function isReadStream(input: unknown): input is Readable {
-  return typeof Object(input).pipe === 'function';
+export function isReadable(input: unknown): input is Readable | ReadableStream {
+  return isLegacyReadableStream(input) || isWebReadableStream(input);
 }
 
-export function isWebStream(input: unknown): input is ReadableStream {
+export function toReadStream(input: Readable | ReadableStream): Readable {
+  return isWebReadableStream(input) ? Readable.fromWeb(input) : input;
+}
+
+function isLegacyReadableStream(input: unknown): input is Readable {
+  return input instanceof Readable;
+}
+
+function isWebReadableStream(input: unknown): input is ReadableStream {
+  // It's important that we don't use `instanceof` here because that would throw
+  // in versions of Node (< 16.5) that don't have web streams.
   return typeof Object(input).getReader === 'function';
 }
