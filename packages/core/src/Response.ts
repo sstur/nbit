01687171e510ca2ae9b77e8e9cb75348a929191c
Simon <sstur@me.com>
2022-07-17 18:16:12 -0700

    Implement src/applicationTypes.ts

--- packages/core/src/Response.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is intentionally just a stub; this will never be instantiated because
- * the files which reference this will only be called from the locations that
- * symlink to `core` meaning, when they import `../Response` they will be
- * getting the real Response (from the parent of they symlink) not this stub.
- */
-export class Response {
-  readonly status: number = 200;
-}
---
+++ packages/node/src/applicationTypes.ts
@@ -0,0 +1,7 @@
+/**
+ * The types in this file represent what will be passed into and what is
+ * expected as return value from each route handler.
+ */
+
+export type { Request } from './Request';
+export type { Response } from './Response';
