ce2e12c381a15515567f1df8e2314d066d87191e
Simon <sstur@me.com>
2022-07-20 22:00:57 -0700

    Build: Also rollup d.ts files

--- packages/bun/rollup.config.js
+++ packages/bun/rollup.config.js
@@ -1,17 +1,28 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'lib',
-    format: 'esm',
-    strict: false,
-    esModule: false,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'lib',
+      format: 'esm',
+      strict: false,
+      esModule: false,
+    },
+    plugins: [
+      typescript({
+        module: 'esnext',
+        include: ['../**/*.ts'],
+      }),
+    ],
   },
-  plugins: [
-    typescript({
-      module: 'esnext',
-      include: ['../**/*.ts'],
-    }),
-  ],
-};
+  {
+    input: 'lib/dts/index.d.ts',
+    output: {
+      file: 'lib/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
--- packages/node/rollup.config.js
+++ packages/node/rollup.config.js
@@ -1,18 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'lib',
-    format: 'cjs',
-    strict: false,
-    esModule: false,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'lib',
+      format: 'cjs',
+      strict: false,
+      esModule: false,
+    },
+    external: ['stream'],
+    plugins: [
+      typescript({
+        module: 'esnext',
+        include: ['../**/*.ts'],
+      }),
+    ],
   },
-  external: ['stream'],
-  plugins: [
-    typescript({
-      module: 'esnext',
-      include: ['../**/*.ts'],
-    }),
-  ],
-};
+  {
+    input: 'lib/dts/index.d.ts',
+    output: {
+      file: 'lib/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
