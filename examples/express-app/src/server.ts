416aa688f0da314b3a2c7e1b266e368324369e7e
Simon <sstur@me.com>
2022-07-25 22:32:14 -0700

    Update examples

--- examples/express-app/src/server.ts
+++ examples/express-app/src/server.ts
@@ -5,11 +5,12 @@ import { attachRoutes } from './application';
 import * as handlers from './handlers';
 
 const app = express();
-const port = 3000;
 
 const middleware = attachRoutes(...Object.values(handlers));
 
 app.use(middleware);
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- examples/node-app/src/server.ts
+++ examples/node-app/src/server.ts
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
+import http from 'http';
+
 import { attachRoutes } from './application';
 import * as handlers from './handlers';
 
-const PORT = 3000;
+const port = 3000;
 
-export default {
-  port: 3000,
-  fetch: attachRoutes(...Object.values(handlers)),
-};
+const server = http.createServer(attachRoutes(...Object.values(handlers)));
 
-console.log(`Server running at http://localhost:${PORT}`);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
