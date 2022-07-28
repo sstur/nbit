a9e1b85171822dc2cb9a6a598761df27ab14ed67
Simon <sstur@me.com>
2022-07-28 16:34:54 -0700

    node: Update src/index.ts

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,4 +1,1 @@
-export { HttpError } from './core';
-export { Response } from './Response';
-// TODO: Export node and express separately
-export { createApplication } from './node';
+export * from './packages/node';
