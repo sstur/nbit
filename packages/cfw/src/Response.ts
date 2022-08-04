5c5db08572dbf02086510fc9b4cf22716414dbe7
Simon <sstur@me.com>
2022-08-03 22:38:23 -0700

    cfw: throw on Request.file()

--- packages/cfw/src/Response.ts
+++ packages/cfw/src/Response.ts
@@ -1,1 +1,7 @@
-export default Response;
+import { type StaticFileOptions } from './core/StaticFile';
+
+export default class CustomResponse extends Response {
+  static file(_filePath: string, _init?: ResponseInit & StaticFileOptions) {
+    throw new Error('Response.file() not implemented.');
+  }
+}
