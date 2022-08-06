3d35551c30e75e392e068b05cf4f66b99aec770e
Simon <sstur@me.com>
2022-08-06 11:10:59 -0500

    node: tests for Request and CustomRequest

---
+++ packages/node/src/__tests__/CustomRequest.test.ts
@@ -0,0 +1,38 @@
+import { Request } from '../Request';
+import { CustomRequest } from '../core/CustomRequest';
+
+describe('CustomRequest', () => {
+  it('should instantiate with full url', async () => {
+    const request = new Request('https://example.com/foo/bar?a=1&b=_');
+    const customRequest = new CustomRequest(request);
+    const { method, url, headers, path, search, query, params } = customRequest;
+    expect({ method, url, path, search, params }).toEqual({
+      method: 'GET',
+      url: 'https://example.com/foo/bar?a=1&b=_',
+      path: '/foo/bar',
+      search: '?a=1&b=_',
+      params: {},
+    });
+    expect(Object.fromEntries(query)).toEqual({ a: '1', b: '_' });
+    expect(Object.fromEntries(headers)).toEqual({});
+  });
+
+  it('should throw if wrong content type', async () => {
+    const request = new Request('/foo', {
+      method: 'post',
+      headers: { 'content-type': 'text/plain' },
+      body: JSON.stringify({ foo: 1 }),
+    });
+    const customRequest = new CustomRequest(request);
+    await expect(customRequest.json()).rejects.toThrow(`Invalid JSON body`);
+  });
+
+  it('should throw if no content type', async () => {
+    const request = new Request('/foo', {
+      method: 'post',
+      body: JSON.stringify({ foo: 1 }),
+    });
+    const customRequest = new CustomRequest(request);
+    await expect(customRequest.json()).rejects.toThrow(`Invalid JSON body`);
+  });
+});
---
+++ packages/node/src/__tests__/Request.test.ts
@@ -0,0 +1,156 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Readable } from 'stream';
+
+import { Request } from '../Request';
+
+describe('Request', () => {
+  it('should construct with just path', () => {
+    const request = new Request('/');
+    expect(request.method).toBe('GET');
+    expect(request.url).toBe('/');
+    expect(Object.fromEntries(request.headers)).toEqual({});
+    expect(request.body).toBe(null);
+    expect(request.bodyRaw).toBe(null);
+  });
+
+  it('should respect method and headers', () => {
+    const request = new Request('/', {
+      method: 'PUT',
+      headers: { foo: 'foo', 'content-type': 'bar' },
+    });
+    expect(request.method).toBe('PUT');
+    expect(request.url).toBe('/');
+    expect(Object.fromEntries(request.headers)).toEqual({
+      foo: 'foo',
+      'content-type': 'bar',
+    });
+    expect(request.body).toBe(null);
+    expect(request.bodyRaw).toBe(null);
+  });
+
+  it('should construct with a null body', () => {
+    const request = new Request('/', { body: null });
+    expect(request.method).toBe('GET');
+    expect(request.url).toBe('/');
+    expect(Object.fromEntries(request.headers)).toEqual({});
+    expect(request.body).toBe(null);
+    expect(request.bodyRaw).toBe(null);
+  });
+
+  it('should construct with a string body', () => {
+    const request = new Request('/', { body: 'foo' });
+    expect(request.method).toBe('GET');
+    expect(request.url).toBe('/');
+    expect(Object.fromEntries(request.headers)).toEqual({});
+    expect(request.body instanceof Readable).toBe(true);
+    expect(request.bodyRaw).toBe('foo');
+  });
+
+  it('should construct with a buffer body', () => {
+    const buffer = Buffer.from('foo');
+    const request = new Request('/foo', { method: 'post', body: buffer });
+    expect(request.method).toBe('POST');
+    expect(Object.fromEntries(request.headers)).toEqual({});
+    expect(request.body instanceof Readable).toBe(true);
+    expect(request.bodyRaw).toBe(buffer);
+  });
+
+  it('should construct with a stream body', async () => {
+    const stream = Readable.from('foo', { objectMode: false });
+    const request = new Request('/foo', { body: stream });
+    expect(Object.fromEntries(request.headers)).toEqual({});
+    expect(request.body).toBe(stream);
+    expect(request.bodyRaw).toBe(stream);
+    expect(request.bodyUsed).toBe(false);
+    const text = await request.text();
+    expect(text).toBe('foo');
+    expect(request.bodyUsed).toBe(true);
+    await expect(request.text()).rejects.toThrow(
+      `TypeError: Failed to execute 'text' on 'Request': body stream already read`,
+    );
+  });
+
+  it('should parse a json body from buffer', async () => {
+    const buffer = Buffer.from(JSON.stringify({ foo: 1 }));
+    const request = new Request('/foo', {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: buffer,
+    });
+    expect(Object.fromEntries(request.headers)).toEqual({
+      'content-type': 'application/json',
+    });
+    expect(request.body instanceof Readable).toBe(true);
+    expect(request.bodyRaw).toBe(buffer);
+    expect(request.bodyUsed).toBe(false);
+    const parsed = await request.json();
+    expect(parsed).toEqual({ foo: 1 });
+    expect(request.bodyUsed).toBe(true);
+    await expect(request.text()).rejects.toThrow(
+      `TypeError: Failed to execute 'text' on 'Request': body stream already read`,
+    );
+  });
+
+  it('should parse a json body from stream', async () => {
+    const stream = Readable.from(JSON.stringify({ foo: 1 }), {
+      objectMode: false,
+    });
+    const request = new Request('/foo', { method: 'get', body: stream });
+    expect(Object.fromEntries(request.headers)).toEqual({});
+    const parsed = await request.json();
+    expect(parsed).toEqual({ foo: 1 });
+    expect(request.bodyUsed).toBe(true);
+    await expect(request.json()).rejects.toThrow(
+      `TypeError: Failed to execute 'json' on 'Request': body stream already read`,
+    );
+  });
+
+  it('should throw if unable to parse json', async () => {
+    const request = new Request('/foo', {
+      method: 'post',
+      body: '[1, 2',
+    });
+    await expect(request.json()).rejects.toThrow(
+      `Unexpected end of JSON input`,
+    );
+  });
+
+  it('should throw if content-length smaller than body size', async () => {
+    const request = new Request('/foo', {
+      method: 'post',
+      headers: { 'content-length': '2' },
+      body: 'foo',
+    });
+    await expect(request.text()).rejects.toThrow(
+      `Request body size did not match content-length header`,
+    );
+  });
+
+  it('should throw if content-length greater than body size', async () => {
+    const request = new Request('/foo', {
+      method: 'post',
+      headers: { 'content-length': '4' },
+      body: 'foo',
+    });
+    await expect(request.text()).rejects.toThrow(
+      `Request body size did not match content-length header`,
+    );
+  });
+
+  it('should throw if body size exceeds maximum allowed', async () => {
+    const request = new Request('/foo', {
+      method: 'post',
+      headers: { 'content-length': '4' },
+      body: 'x'.repeat(1001),
+      options: { bodyParserMaxBufferSize: 1000 },
+    });
+    let error: any;
+    try {
+      await request.text();
+    } catch (e) {
+      error = e;
+    }
+    expect(error.status).toBe(413);
+    expect(error.message).toBe('Request Entity Too Large');
+  });
+});
