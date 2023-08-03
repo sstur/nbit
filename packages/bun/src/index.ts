f0a7e0afb70de7f80942b056909bc71fbc20195d
Simon <sstur@me.com>
2023-08-03 10:15:51 +0700

    bun: export Request (for consistency; even though it's already a global)

--- packages/bun/src/index.ts
+++ packages/bun/src/index.ts
@@ -1,3 +1,4 @@
 export { HttpError } from './core';
+export { default as Request } from './builtins/Request';
 export { default as Response } from './Response';
 export { createApplication } from './bun';
