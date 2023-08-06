7db60c2ecbdb30dd1dbe3ba66d8db74bd48ae94b
Simon <sstur@me.com>
2023-08-06 16:10:14 -0700

    [core] Minor update to parseUrl

--- packages/core/src/core/support/parseUrl.ts
+++ packages/core/src/core/support/parseUrl.ts
@@ -1,7 +1,7 @@
-// This is required by `new URL()` for parsing but it doesn't matter what value
-// this has since it's not used.
+// This is required by `new URL()` if the url is not fully-qualified (e.g. just
+// a pathname). In most cases it doesn't matter what value we use here.
 // See: https://nodejs.org/docs/latest-v12.x/api/url.html#url_url
-const URL_BASE = 'https://0.0.0.0';
+const URL_BASE = 'http://0.0.0.0';
 
 export function parseUrl(url: string) {
   return new URL(url, URL_BASE);
